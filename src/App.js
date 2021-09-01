294cb0e6d33e927d5365ee5ffbafd19d4bfe2e7c
Lillian Paris <lillian.paris529@gmail.com>
2021-09-01 14:29:36 -0700

    Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Home from "./pages/Home"
 
 function App() {
   return (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navbar/navbar";
+
+
 
 function Home() {
     return (
