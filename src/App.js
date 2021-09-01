15aecc4f5577a8ff70965096852948daa738788f
Lillian Paris <lillian.paris529@gmail.com>
2021-09-01 14:26:30 -0700

    Downloaded react render dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    
+
  <Router>
 <Route exact path="/" component={Home}/>
  </Router>
