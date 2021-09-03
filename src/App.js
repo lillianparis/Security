60456620e02c554ab2405e344607ad47d2a98fe9
Lillian Paris <lillian.paris529@gmail.com>
2021-09-03 13:19:15 -0700

    Editing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar/navbar';
 import Home from "./pages/Home"
 
 
@@ -8,6 +10,7 @@ function App() {
   return (
 
  <Router>
+   <Navbar/>
 <Route exact path="/" component={Home}/>
  </Router>
 
